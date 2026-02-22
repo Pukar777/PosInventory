35ef008adee17829a5c090d024cc43057ebf1f8c
pukar <pukar@local>
2026-02-23 03:46:00 +0545

    patch(redirecting url) :any request to web url will be redirected to frontend url

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('{any}', function () {
+    return redirect(env('FRONTEND_URL', 'http://localhost:5173'));
+})->where('any', '.*');
