c9753fc078333bc858883ab4ea47ea1cb7133346
pukar <pukar@local>
2026-02-23 02:41:02 +0545

    patch(customHook useApi) :appending /api on base url defined in env

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -3,7 +3,7 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { useCallback } from 'react';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || `http://${window.location.hostname}:8000/api`,
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
